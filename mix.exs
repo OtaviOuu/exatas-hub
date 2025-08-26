95a7ca7917fe3fd2b2390c35da905cc0ad0b00ba
otaviouu <otavio.venancio.oliveira@usp.br>
2025-08-26 09:59:51 -0300

    setup magic link

--- mix.exs
+++ mix.exs
@@ -61,7 +61,8 @@ defmodule ExatasHub.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:hackney, "~> 1.9"}
     ]
   end
 
