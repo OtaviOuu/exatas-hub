a6a4ace8b95e52f62e6bb63ccd33b65d3407ab55
otaviouu <otavio.venancio.oliveira@usp.br>
2025-08-27 13:41:30 -0300

    refactor course card layout and styling for improved responsiveness and consistency

--- lib/exatas_hub_web/live/course_live/index.ex
+++ lib/exatas_hub_web/live/course_live/index.ex
@@ -16,9 +16,9 @@ defmodule ExatasHubWeb.CourseLive.Index do
   def render(assigns) do
     ~H"""
     <Layouts.app current_scope={@current_scope} flash={@flash}>
-      <h1>Courses</h1>
+      <h1 class="text-2xl font-bold text-base-content mb-4">Courses</h1>
       <section
-        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6"
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6 px-3 sm:px-0"
         id="courses-grid"
         phx-update="stream"
       >
@@ -31,9 +31,10 @@ defmodule ExatasHubWeb.CourseLive.Index do
   def course_card(assigns) do
     ~H"""
     <div
-      class="card w-80 bg-base-100 shadow-md rounded-xl transition-transform duration-300 hover:scale-105 hover:shadow-xl"
       id={@id}
+      class="card w-full sm:w-auto bg-base-100 shadow-md rounded-xl transition-transform duration-300 hover:scale-105 hover:shadow-xl"
     >
+      <!-- Imagem -->
       <figure class="relative w-full aspect-video overflow-hidden rounded-t-xl">
         <.link navigate={~p"/courses/#{@course.slug}"}>
           <img
@@ -43,18 +44,20 @@ defmodule ExatasHubWeb.CourseLive.Index do
           />
         </.link>
       </figure>
-      <div class="card-body p-4 space-y-2">
+      
+    <!-- Conteúdo -->
+      <div class="card-body p-3 sm:p-4 space-y-2">
         <div class="flex items-center gap-3">
           <img
             src={@course.university.logo}
             alt="Universidade"
-            class="w-7 h-7 rounded bg-base-200 border"
+            class="w-6 h-6 sm:w-7 sm:h-7 rounded bg-base-200 border"
           />
-          <h2 class="card-title text-base font-semibold leading-snug flex-1 truncate">
+          <h2 class="card-title text-sm sm:text-base font-semibold leading-snug flex-1 truncate">
             {@course.title}
           </h2>
         </div>
-        <p class="text-sm text-gray-600 line-clamp-2">
+        <p class="text-xs sm:text-sm text-gray-600 line-clamp-2">
           A card component has a figure, a body part, and inside body there are title and actions parts
         </p>
       </div>
